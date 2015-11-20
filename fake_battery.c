01d370089a547b2fadbd65ed64335ace1d4385f9
Rob Hoelz <rob@hoelz.ro>
2015-11-20 08:52:47 -0600

    Fix indent and other formatting

--- fake_battery.c
+++ fake_battery.c
@@ -1,19 +1,20 @@
 /*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /* Based heavily on https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/tree/drivers/power/test_power.c?id=refs/tags/v4.2.6 */
+
 
 #include <linux/fs.h>
 #include <linux/kernel.h>
@@ -98,23 +99,23 @@ static struct miscdevice control_device = {
 };
 
 static enum power_supply_property fake_battery_properties[] = {
-        POWER_SUPPLY_PROP_STATUS,
-        POWER_SUPPLY_PROP_CHARGE_TYPE,
-        POWER_SUPPLY_PROP_HEALTH,
-        POWER_SUPPLY_PROP_PRESENT,
-        POWER_SUPPLY_PROP_TECHNOLOGY,
-        POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN,
-        POWER_SUPPLY_PROP_CHARGE_FULL,
-        POWER_SUPPLY_PROP_CHARGE_NOW,
-        POWER_SUPPLY_PROP_CAPACITY,
-        POWER_SUPPLY_PROP_CAPACITY_LEVEL,
-        POWER_SUPPLY_PROP_TIME_TO_EMPTY_AVG,
-        POWER_SUPPLY_PROP_TIME_TO_FULL_NOW,
-        POWER_SUPPLY_PROP_MODEL_NAME,
-        POWER_SUPPLY_PROP_MANUFACTURER,
-        POWER_SUPPLY_PROP_SERIAL_NUMBER,
-        POWER_SUPPLY_PROP_TEMP,
-        POWER_SUPPLY_PROP_VOLTAGE_NOW,
+    POWER_SUPPLY_PROP_STATUS,
+    POWER_SUPPLY_PROP_CHARGE_TYPE,
+    POWER_SUPPLY_PROP_HEALTH,
+    POWER_SUPPLY_PROP_PRESENT,
+    POWER_SUPPLY_PROP_TECHNOLOGY,
+    POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN,
+    POWER_SUPPLY_PROP_CHARGE_FULL,
+    POWER_SUPPLY_PROP_CHARGE_NOW,
+    POWER_SUPPLY_PROP_CAPACITY,
+    POWER_SUPPLY_PROP_CAPACITY_LEVEL,
+    POWER_SUPPLY_PROP_TIME_TO_EMPTY_AVG,
+    POWER_SUPPLY_PROP_TIME_TO_FULL_NOW,
+    POWER_SUPPLY_PROP_MODEL_NAME,
+    POWER_SUPPLY_PROP_MANUFACTURER,
+    POWER_SUPPLY_PROP_SERIAL_NUMBER,
+    POWER_SUPPLY_PROP_TEMP,
+    POWER_SUPPLY_PROP_VOLTAGE_NOW,
 };
 
 static struct battery_status {
@@ -131,74 +132,76 @@ static struct battery_status {
     },
 };
 
-static int fake_battery_generic_get_property(struct power_supply *psy,
-                                           enum power_supply_property psp,
-                                           union power_supply_propval *val,
-                                           struct battery_status *status)
+static int
+fake_battery_generic_get_property(struct power_supply *psy,
+        enum power_supply_property psp,
+        union power_supply_propval *val,
+        struct battery_status *status)
 {
-        switch (psp) {
+    switch (psp) {
         case POWER_SUPPLY_PROP_MANUFACTURER:
-                val->strval = "Linux";
-                break;
+            val->strval = "Linux";
+            break;
         case POWER_SUPPLY_PROP_STATUS:
-                val->intval = status->status;
-                break;
+            val->intval = status->status;
+            break;
         case POWER_SUPPLY_PROP_CHARGE_TYPE:
-                val->intval = POWER_SUPPLY_CHARGE_TYPE_FAST;
-                break;
+            val->intval = POWER_SUPPLY_CHARGE_TYPE_FAST;
+            break;
         case POWER_SUPPLY_PROP_HEALTH:
-                val->intval = POWER_SUPPLY_HEALTH_GOOD;
-                break;
+            val->intval = POWER_SUPPLY_HEALTH_GOOD;
+            break;
         case POWER_SUPPLY_PROP_PRESENT:
-                val->intval = 1;
-                break;
+            val->intval = 1;
+            break;
         case POWER_SUPPLY_PROP_TECHNOLOGY:
-                val->intval = POWER_SUPPLY_TECHNOLOGY_LION;
-                break;
+            val->intval = POWER_SUPPLY_TECHNOLOGY_LION;
+            break;
         case POWER_SUPPLY_PROP_CAPACITY_LEVEL:
-                val->intval = status->capacity_level;
-                break;
+            val->intval = status->capacity_level;
+            break;
         case POWER_SUPPLY_PROP_CAPACITY:
         case POWER_SUPPLY_PROP_CHARGE_NOW:
-                val->intval = status->capacity;
-                break;
+            val->intval = status->capacity;
+            break;
         case POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN:
         case POWER_SUPPLY_PROP_CHARGE_FULL:
-                val->intval = 100;
-                break;
+            val->intval = 100;
+            break;
         case POWER_SUPPLY_PROP_TIME_TO_EMPTY_AVG:
         case POWER_SUPPLY_PROP_TIME_TO_FULL_NOW:
-                val->intval = status->time_left;
-                break;
+            val->intval = status->time_left;
+            break;
         case POWER_SUPPLY_PROP_TEMP:
-                val->intval = 26;
-                break;
+            val->intval = 26;
+            break;
         case POWER_SUPPLY_PROP_VOLTAGE_NOW:
-                val->intval = 3300;
-                break;
+            val->intval = 3300;
+            break;
         default:
-                pr_info("%s: some properties deliberately report errors.\n",
-                        __func__);
-                return -EINVAL;
-        }
-        return 0;
+            pr_info("%s: some properties deliberately report errors.\n",
+                    __func__);
+            return -EINVAL;
+    }
+    return 0;
 };
 
-static int fake_battery_get_property1(struct power_supply *psy,
-                                           enum power_supply_property psp,
-                                           union power_supply_propval *val)
+static int
+fake_battery_get_property1(struct power_supply *psy,
+        enum power_supply_property psp,
+        union power_supply_propval *val)
 {
-        switch (psp) {
+    switch (psp) {
         case POWER_SUPPLY_PROP_MODEL_NAME:
-                val->strval = "Fake battery 1";
-                break;
+            val->strval = "Fake battery 1";
+            break;
         case POWER_SUPPLY_PROP_SERIAL_NUMBER:
-                val->strval = "12345678";
-                break;
+            val->strval = "12345678";
+            break;
         default:
-                return fake_battery_generic_get_property(psy, psp, val, &fake_battery_statuses[0]);
-        }
-        return 0;
+            return fake_battery_generic_get_property(psy, psp, val, &fake_battery_statuses[0]);
+    }
+    return 0;
 }
 
 static struct power_supply_desc fake_battery1 = {
